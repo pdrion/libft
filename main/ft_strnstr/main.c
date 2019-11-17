4ea066fe1d7dc9d1f2f8a3eeb5cbc1dffe546045
pdrion <lazrossi@student.42.fr>
2019-11-17 21:54:44 +0100

    je teste magit

--- main/ft_strnstr/main.c
+++ main/ft_strnstr/main.c
@@ -15,25 +15,10 @@
 #include "libft.h"
 int main (int argc, char **argv)
 {
-	if (argc > 2)
-	{
-		char	*ar_1;
-		const char	*ar_2;
-		size_t		ar_3;
-		
-		ar_1 = argv[1];
-		ar_2 = argv[2];
-		ar_3 = atoi(argv[3]);
-
-		printf("la taille est = %zu \n",strlen(ar_2));
-		printf("la dest = %s \n",ar_1);
-		strlcat(ar_1, ar_2, ar_3);
-		printf("dest = %s \n\n", ar_1);
-		printf("la taille max = %zu \n", strlen("the cake is a lie !\0I'm hidden lol\r\n") + strlen("there is no stars in the sky"));
-		printf("la taille max = %zu \n", strlen("there is no stars in the skythe cake is a lie "));
-
-
-//		printf("ma fonction       : %lu\n",ft_strlcpy(ar_1, ar_2, ar_3));
-	}
-	return (0);
+	char *s1 = "AAAAAAAAAAAAA";
+	size_t max = strlen(s1);
+	char *i1 = strnstr(s1, s1, max);
+	char *i2 = ft_strnstr(s1, s1, max);
+	printf("Good result: %s \n", *i1);
+	printf("My result:", *i2);
 }
